b754003f02e038e5aa6e79d91d6a0385292ed985
Jerryaa98 <82344133+Jerryaa98@users.noreply.github.com>
2022-09-05 11:20:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import ISS_Info
+import turtle
+import time
+
+
+def Track():
+    screen = turtle.Screen()
+    screen.setup(720,360)
+    screen.setworldcoordinates(-180,-90,180,90)
+    screen.bgpic("world.png")
+    screen.register_shape("iss.gif")
+
+    iss = turtle.Turtle()
+    iss.shape("iss.gif")
+    iss.penup()
+
+    while True:
+        location = ISS_Info.iss_current_loc()
+        lat = location['iss_position']['latitude']
+        lon = location['iss_position']['longitude']
+        print("Position: \n latitude: {}, longitude: {}".format(lat,lon))
+        iss.goto(float(lon),float(lat))
+        time.sleep(10)
+
+if __name__ == '__main__':
+   Track()
+
